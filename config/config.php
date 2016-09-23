0876b07a77e99f1baa14d44d94e68634615d58b5
Konrad <vccvkonrad@hotmail.com>
2016-09-23 22:43:33 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,38 @@
+﻿
+<?php
+/*
+ _   __                          _ 
+| | / /                         | |
+| |/ /  ___  _ __  _ __ __ _  __| |
+|    \ / _ \| '_ \| '__/ _` |/ _` |
+| |\  \ (_) | | | | | | (_| | (_| |
+\_| \_/\___/|_| |_|_|  \__,_|\__,_|
+
+*/
+
+//EN - FR
+//CONFIGURATION - CONFIG
+
+
+//SITE NAME - Nom du site
+$sitename = 'SITE NAME - Nom du site';
+
+
+// CAPS ONLY - Majuscule seulement
+
+
+//Bit or byte - Bit ou octet
+// US=bit and/et EU= octet
+$num = 'EU';
+
+//Data transferred or received  -  Donnée transférée ou recus
+//RX = received/recus TX = transferred/transféerée
+$datatype = 'TX';
+
+//Language - Langue
+//FR = francais/french - EN = english/anglais
+$lang = 'FR';
+
+//IP OF SERVER - Ip du serveur
+$ip = '127.0.0.1';
+?>
